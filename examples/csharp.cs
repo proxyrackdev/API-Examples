295b7c424edfd806da055cfb1f13bd3b9e454689
Matt <33204743+Laiteux@users.noreply.github.com>
2020-03-16 14:59:39 +1100

    Updated C# example: Use of HttpClient

--- examples/csharp.cs
+++ examples/csharp.cs
@@ -1,40 +1,31 @@
 ﻿using System;
-using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ProxyRackHttpExample
 {
-    class Program
+    public static class Program
     {
         private const string Username = "YOURUSERNAME";
         private const string Password = "YOURPASSWORD";
-
-        private const string PROXY_RACK_DNS = "http://megaproxy.rotating.proxyrack.net:222";
-
+        private const string ProxyRackDns = "http://megaproxy.rotating.proxyrack.net:222";
         private const string UrlToGet = "http://ip-api.com/json";
 
-        static void Main(string[] args)
+        public static async Task Main()
         {
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(UrlToGet);
-            var webProxy =
-                new WebProxy(new Uri(PROXY_RACK_DNS))
-                {
-                    Credentials = new NetworkCredential(Username, Password)
-                };
-
-            httpWebRequest.Proxy = webProxy;
-
-
-            var httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            var responseStream = httpWebResponse.GetResponseStream();
-            if (responseStream == null)
+            using var httpClient = new HttpClient(new HttpClientHandler
             {
-                return;
-            }
+                Proxy = new WebProxy(ProxyRackDns),
+                Credentials = new NetworkCredential(Username, Password)
+            });
 
-            var responseString = new StreamReader(responseStream).ReadToEnd();
-            Console.WriteLine($"Response:\n{responseString}");
-            Console.ReadKey();
+            using var responseMessage = await httpClient.GetAsync(UrlToGet);
+
+            var contentString = await responseMessage.Content.ReadAsStringAsync();
+
+            Console.WriteLine("Response:" + Environment.NewLine + contentString);
+            Console.ReadKey(true);
         }
     }
 }
--- examples/php.php
+++ examples/php.php
@@ -1,20 +1,23 @@
 <?php
+
 $username = 'YOURUSERNAME';
 $password = 'YOURPASSWORD';
 $PROXY_RACK_PORT = 222;
 $PROXY_RACK_DNS = 'megaproxy.rotating.proxyrack.net';
 
 $urlToGet = 'http://ip-api.com/json';
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $urlToGet);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_PROXYPORT, $PROXY_RACK_PORT);
-    curl_setopt($ch, CURLOPT_PROXYTYPE, 'HTTP');
-    curl_setopt($ch, CURLOPT_PROXY, $PROXY_RACK_DNS);
-    curl_setopt($ch, CURLOPT_PROXYUSERPWD, $username.':'.$password);
-    $data = curl_exec($ch);
-    curl_close($ch);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $urlToGet);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_PROXYPORT, $PROXY_RACK_PORT);
+curl_setopt($ch, CURLOPT_PROXYTYPE, 'HTTP');
+curl_setopt($ch, CURLOPT_PROXY, $PROXY_RACK_DNS);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $username.':'.$password);
+$data = curl_exec($ch);
+curl_close($ch);
 
 echo $data;
+
 ?>
